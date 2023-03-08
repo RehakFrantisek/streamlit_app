89cbbf45fe5b34ac013e8b8e03fd60b586615878
RehakFrantisek <62970750+RehakFrantisek@users.noreply.github.com>
2023-03-08 09:37:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,8 @@ streamlit.text('🥑🍞 Avo toas')
 
 
 import pandas
+from urllib.error import URLError
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -23,6 +25,8 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
+streamlit.write('The user entered ', fruit_choice)
+
 try:
   if not fruit_choice:
     streamlit.error("Select fruit")
@@ -33,8 +37,7 @@ try:
 except URLError as e:
   streamlit.error()  
   
-streamlit.write('The user entered ', fruit_choice)
-
+  
 import requests
 fruity_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
@@ -42,7 +45,7 @@ streamlit.text(fruity_response.json())
 
 # write your own comment -what does the next line do? 
 
-
+streamlit.stop()
 
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
