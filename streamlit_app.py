dfe0735003ef72327cbb28f63023bbe635c5ea67
RehakFrantisek <62970750+RehakFrantisek@users.noreply.github.com>
2023-03-27 12:23:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,6 +64,16 @@ streamlit.text(fruity_response.json())
 
 # write your own comment -what does the next line do? 
 
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list containts")
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_rows)
+
+
 streamlit.stop()
 
 
